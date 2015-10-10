75e29a9d61e59b9c14d994b358ab19e034367178
Bayo Fodeke <bfodeke@gmail.com>
2015-10-09 21:40:28 -0400

    Alterations to Location Map module

--- sites/all/modules/custom/location_map/hb5-location-map.tpl.php
+++ sites/all/modules/custom/location_map/hb5-location-map.tpl.php
@@ -12,20 +12,40 @@ drupal_add_js(array('location_map' => array('module_path' => $module_path)), arr
 ?>
 
 <div id="hb5-location-map"></div>
-<div id="location-text" class="container">
-  <div class="location-text-content col-md-5 col-md-offset-7">
-    <div class="text-center location-icon-large">
-      <span class="glyphicon glyphicon-flag"></span>
-    </div>
-    <h2>Stop by and see us!</h2>
-    <address>
-      <strong>hb5 cowork</strong><br>
-      42 Union Street S.<br>
-      Concord, NC 28025<br>
+<div id="location-text-wrapper">
+  <div class="container">
+    <div class="location-text-content col-md-5 col-md-offset-7">
+      <h2>Stop by and see us!</h2>
+      <address>
+        <strong>hb5 cowork</strong><br>
+        42 Union Street S.<br>
+        Concord, NC 28025<br>
 
-      <p>Shoot us an <a href="mailto:cowork@hb5.co">email</a></p>
-    </address>
+        <p>Shoot us an <a href="mailto:cowork@hb5.co">email</a></p>
+        <hr />
+        <ul class="list-inline social-links">
+          <li class="facebook">
+            <a rel="nofollow" href="http://facebook.com/hb5cowork">
+              <i class="fa fa-facebook-square"></i>
+              <span class="social-text">Facebook</span>
+            </a>
+          </li>
+          <li class="instagram">
+            <a rel="nofollow" href="http://instagram.com/hb5cowork">
+              <i class="fa fa-instagram"></i>
+              <span class="social-text">Instagram</span>
+            </a>
+          </li>
+          <li class="twitter">
+            <a rel="nofollow" href="http://twitter.com/hb5cowork">
+              <i class="fa fa-twitter"></i>
+              <span class="social-text">Twitter</span>
+            </a>
+          </li>
+        </ul>
+      </address>
 
+      </div>
     </div>
   </div>
 </div>
